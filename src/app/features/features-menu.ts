5115c298edbd09342eafccdebaf5b4a979933016
madricks <achmadricky96@gmail.com>
2018-05-14 10:44:28 +0700

    Change menu

--- src/app/features/features-menu.ts
+++ src/app/features/features-menu.ts
@@ -2,24 +2,22 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
+    title: 'Dashboard',
+    icon: 'nb-home',
+    link: '/pages/dashboard',
+    home: true,
+  },
+  {
+    title: 'FEATURES',
+    group: true,
+  },
+  {
     title: 'Store',
     icon: 'nb-layout-default',
     children: [
       {
         title: 'list',
         link: '/store/list',
-      },
-      {
-        title: 'Register',
-        link: '/auth/register',
-      },
-      {
-        title: 'Request Password',
-        link: '/auth/request-password',
-      },
-      {
-        title: 'Reset Password',
-        link: '/auth/reset-password',
       },
     ],
   },
