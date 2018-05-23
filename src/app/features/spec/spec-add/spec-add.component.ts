4e692e87b97da5c3057d41aa5018a5647f5532d8
Lalang <lalanggumirang@gmail.com>
2018-05-23 14:59:07 +0700

    spec

--- src/app/features/spec/spec-add/spec-add.component.ts
+++ src/app/features/spec/spec-add/spec-add.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import swal from 'sweetalert2';
+import { ShareMessageService } from './../../../@core/services/share-message/share-message.service';
+import { SpecService } from './../../../@core/services/spec/spec.service';
 
 @Component({
   selector: 'spec-add',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpecAddComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public shareMessageService: ShareMessageService,
+    public specService: SpecService
+  ) { }
 
   ngOnInit() {
+  }
+
+  name: string;
+
+  save(){
+    if(this.name=='' || this.name==undefined){
+      swal(
+        'Alert',
+        'Nama tidak boleh kosong',
+        'error',
+      );
+    }else{
+
+      const data = {
+        "name": this.name,
+        "isMandatory":true,
+        "isInstanceAttribute":false
+      }
+
+      this.specService.add(data).subscribe(response => {
+        if(response.status == 1) {
+          swal(
+            'Alert',
+            response.message,
+            'success',
+          );
+          this.shareMessageService.changeMessage('add-brand');
+        }else{
+          swal(
+            'Alert',
+            response.message,
+            'error',
+          );
+        }
+        
+      });
+    }
   }
 
 }
--- src/app/features/spec/spec.module.ts
+++ src/app/features/spec/spec.module.ts
@@ -1,19 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { SpecRoutingModule } from './spec-routing.module';
 import { SpecComponent } from './spec.component';
 import { SpecListComponent } from './spec-list/spec-list.component';
 import { SpecAddComponent } from './spec-add/spec-add.component';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ThemeModule } from '../../@theme/theme.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    SpecRoutingModule,
     ThemeModule,
     FormsModule,
-    ReactiveFormsModule,
-    SpecRoutingModule
+    ReactiveFormsModule
   ],
   declarations: [SpecComponent, SpecListComponent, SpecAddComponent]
 })
