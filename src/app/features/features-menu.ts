ac99dd9af85932fa53567f971a0a8415a41a911b
wahyu <wahyu@wahyus-MacBook-Pro.local>
2018-10-02 13:41:15 +0700

    fixing courir menjadi courier

--- src/app/features/features-menu.ts
+++ src/app/features/features-menu.ts
@@ -100,12 +100,12 @@ export const MENU_ITEMS: NbMenuItem[] = [
     ],
   },
   {
-    title: 'Courir',
+    title: 'Courier',
     icon: 'nb-layout-default',
     children: [
       {
         title: 'List',
-        link: '/courir/list',
+        link: '/courier/list',
       },
     ],
   },
