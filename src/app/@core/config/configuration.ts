009e8244d61501caf1713b3a27d0fd0cd1aea3c3
Achmad Ricky Budianto <achmadricky96@gmail.com>
2018-12-12 11:22:15 +0700

    [MIGRATION] Remove older admin console version

--- src/app/@core/config/configuration.ts
+++
@@ -1,8 +0,0 @@
-import { environment } from './../../../environments/environment';
-import { Injectable } from '@angular/core';
-
-@Injectable()
-
-export class Configuration {
-  apiURL: string = environment.apiUrl;
-}
