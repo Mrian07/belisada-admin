e695df1687fd90b59ac6b24d0f3e4692d4a18e3b
Achmad Ricky Budianto <achmadricky96@gmail.com>
2018-12-12 16:36:15 +0700

    [Migration] remove old version admin console

--- src/app/features/manage-buyer/manage-buyer.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'bsa-manage-buyer',
-  template: `<router-outlet></router-outlet>`,
-})
-export class ManageBuyerComponent {
-
-}
--- src/app/features/manage-shipment/manage-shipment.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'bsa-manage-shipment',
-  template: `<router-outlet></router-outlet>`,
-})
-export class ManageShipmentComponent {
-
-}
