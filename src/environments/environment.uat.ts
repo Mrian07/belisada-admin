5077316d56b561e2da45c08d4d263d2dd59f2703
Achmad Ricky Budianto <achmadricky96@gmail.com>
2019-03-20 10:43:00 +0700

    uat enfironment

---
+++ src/environments/environment.uat.ts
@@ -0,0 +1,17 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.uat.belisada.id/belisada-admin',
+  firebase: {
+    apiKey: 'AIzaSyDHdXsPrLfNpCMKbdrLSsL2r3XD8MB6Hbs',
+    authDomain: 'belisada-id.firebaseapp.com',
+    databaseURL: 'https://belisada-id.firebaseio.com',
+    projectId: 'belisada-id',
+    storageBucket: 'belisada-id.appspot.com',
+    messagingSenderId: '1065104193441'
+  }
+};
